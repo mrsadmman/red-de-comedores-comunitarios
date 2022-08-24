102888acd24e3e39be98a060c0e1ddbf53d28e71
Juan Taphanel <taphanel.juan@gmail.com>
2022-08-24 11:56:51 -0300

    manejo de formularios con eventos

--- main.js
+++ main.js
@@ -11,19 +11,39 @@ const comedor3 = new Comedor("Doña Tota", 60, "Villa Fiorito");
 
 //const comedores = [comedor1, comedor2, comedor3];
 
-/* AGREGAR UN NUEVO COMEDOR */
-let miFormulario = document.getElementById("formulario");
-miFormulario.addEventListener("submit", validarFormulario);
+/* AGREGAR UN NUEVO COMEDOR  PESTAÑA COMEDORES*/
+const miFormulario = document.getElementById("formulario");
 
-function validarFormulario(e) {
+const nombreComedor = document.getElementById("nombreComedor");
+const cantidad = document.getElementById("cantidad");
+const direc = document.getElementById("direc");
+const btnGuardar = document.getElementById("btnGuardar");
+
+const nuevoComedor = [];
+
+btnGuardar.addEventListener("click", agregarComedor);
+
+function agregarComedor(e) {
   //Cancelamos el comportamiento del evento
   e.preventDefault();
-  //Obtenemos el elemento desde el cual se disparó el evento
-  let formulario = e.target;
-  //Obtengo el valor del primero hijo <input type="text">
-  console.log(document.getElementById("nombre").value);
-  //Obtengo el valor del segundo hijo <input type="number">
-  console.log(document.getElementById("cantidad").value);
-  console.log(document.getElementById("direc").value);
-  console.log(document.getElementById("formulario"));
+
+  if (nombreComedor.value && cantidad.value && direc.value) {
+    const newHTML = document.createElement("p");
+    newHTML.innerHTML =
+      "<h2>Se registro un nuevo comedor</h2> <br>  <h2>Los Datos son:</h2> " +
+      "<h3>Nombre Del Comedor</h3> <br>" +
+      nombreComedor.value +
+      "<h3>Cantidad de Comensales</h3> <br>" +
+      cantidad.value +
+      "<h3>Ubicacion</h3> <br>" +
+      direc.value;
+    miFormulario.append(newHTML);
+  } else {
+    alert("Completa el formulario");
+  }
+
+  nombreComedor.value = "";
+  cantidad.value = "";
+  direc.value = "";
 }
+/* AGREGAR UN NUEVO COMEDOR  PESTAÑA COMEDORES */

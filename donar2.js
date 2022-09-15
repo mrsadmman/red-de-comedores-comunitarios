46c6792a68119a372ae4163e311348af7f93467d
Juan Taphanel <taphanel.juan@gmail.com>
2022-09-14 23:26:13 -0300

    Utilizando fetch para enviar mail y datos locales, y agregando libreria sweet alert

--- donar2.js
+++ donar2.js
@@ -1,55 +1,3 @@
-const baseDeDatos = [
-  {
-    id: 1,
-    nombre: 'El Gomero',
-    precio: 1,
-    ubicacion: 'Barrancas de Belgrano',
-    comensales: 50,
-    imagen: './images/logos/elgomero.jpg',
-  },
-  {
-    id: 2,
-    nombre: 'CCC',
-    precio: 1,
-    comensales: 70,
-    ubicacion: 'Barrio Mitre',
-    imagen: './images/logos/ccc.jpg',
-  },
-  {
-    id: 3,
-    nombre: 'La Poderosa Fatima',
-    precio: 1,
-    ubicacion: 'Villa Fatima',
-    comensales: 90,
-
-    imagen: './images/logos/lapoderosa.png',
-  },
-  {
-    id: 4,
-    nombre: 'Don Segundo Sombra',
-    precio: 1,
-    comensales: 60,
-    ubicacion: 'Parque Patricios',
-    imagen: './images/logos/donsegundosombra.jpg',
-  },
-  {
-    id: 5,
-    nombre: 'Amanecer de Los Cartoneros',
-    precio: 1,
-    comensales: 120,
-    ubicacion: 'Parque Patricios',
-    imagen: './images/logos/cartoneros.jpg',
-  },
-  {
-    id: 6,
-    nombre: 'Pelusa',
-    precio: 1,
-    comensales: 100,
-    ubicacion: 'La Carbonilla',
-    imagen: './images/logos/pelusa.jpg',
-  },
-];
-
 let carrito = [];
 const divisa = ' Vianda';
 const comensa = ' Comensales';
@@ -64,54 +12,59 @@ const DOMbotonVaciar = document.querySelector('#boton-vaciar');
  * Dibuja todos los productos a partir de la base de datos. No confundir con el carrito
  */
 function renderizarProductos() {
-  baseDeDatos.forEach((info) => {
-    // Estructura
-    const miNodo = document.createElement('div');
-    miNodo.classList.add('card', 'col-sm-4');
-    // Body
-    const miNodoCardBody = document.createElement('div');
-    miNodoCardBody.classList.add('card-body');
-    // Titulo
-    const miNodoTitle = document.createElement('h5');
-    miNodoTitle.classList.add('card-title');
-    miNodoTitle.textContent = info.nombre;
-    // Imagen
-    const miNodoImagen = document.createElement('img');
-    miNodoImagen.classList.add('img-fluid');
-    miNodoImagen.setAttribute('src', info.imagen);
-    // Comensales
-    const miNodoComensales = document.createElement('p');
-    miNodoComensales.classList.add('card-text');
-    miNodoComensales.textContent = `${info.comensales}${comensa}`;
-    // Ubicacion
-    const miNodoUbicacion = document.createElement('h6');
-    miNodoUbicacion.classList.add('card-text');
-    miNodoUbicacion.textContent = `${info.ubicacion}`;
-    // Precio
-    const miNodoPrecio = document.createElement('p');
-    miNodoPrecio.classList.add('card-text');
-    miNodoPrecio.textContent = `${info.precio}${divisa}`;
-    // Boton
-    const miNodoBoton = document.createElement('button');
-    miNodoBoton.classList.add('btn', 'btn-primary');
-    miNodoBoton.textContent = '+';
-    miNodoBoton.setAttribute('marcador', info.id);
-    miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
-    // Insertamos
-    miNodoCardBody.appendChild(miNodoImagen);
-    miNodoCardBody.appendChild(miNodoTitle);
-    miNodoCardBody.appendChild(miNodoComensales);
-    miNodoCardBody.appendChild(miNodoUbicacion);
-    miNodoCardBody.appendChild(miNodoPrecio);
-    miNodoCardBody.appendChild(miNodoBoton);
-    miNodo.appendChild(miNodoCardBody);
-    DOMitems.appendChild(miNodo);
-  });
+  fetch('data.json')
+    .then((res) => res.json())
+    .then((json) => {
+      json.forEach((info) => {
+        // Estructura
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        // Body
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        // Titulo
+        const miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+        // Imagen
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info.imagen);
+        // Comensales
+        const miNodoComensales = document.createElement('p');
+        miNodoComensales.classList.add('card-text');
+        miNodoComensales.textContent = `${info.comensales}${comensa}`;
+        // Ubicacion
+        const miNodoUbicacion = document.createElement('h6');
+        miNodoUbicacion.classList.add('card-text');
+        miNodoUbicacion.textContent = `${info.ubicacion}`;
+        // Precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = `${info.precio}${divisa}`;
+        // Boton
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+        // Insertamos
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoComensales);
+        miNodoCardBody.appendChild(miNodoUbicacion);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
+renderizarProductos();
 
-/**
- * Evento para añadir un producto al carrito de la compra
- */
 function anyadirProductoAlCarrito(evento) {
   // Anyadimos el Nodo a nuestro carrito
   carrito.push(evento.target.getAttribute('marcador'));
@@ -123,39 +76,45 @@ function anyadirProductoAlCarrito(evento) {
  * Dibuja todos los productos guardados en el carrito
  */
 function renderizarCarrito() {
-  // Vaciamos todo el html
-  DOMcarrito.textContent = '';
-  // Quitamos los duplicados
-  const carritoSinDuplicados = [...new Set(carrito)];
-  // Generamos los Nodos a partir de carrito
-  carritoSinDuplicados.forEach((item) => {
-    // Obtenemos el item que necesitamos de la variable base de datos
-    const miItem = baseDeDatos.filter((itemBaseDatos) => {
-      // ¿Coincide las id? Solo puede existir un caso
-      return itemBaseDatos.id === parseInt(item);
+  fetch('data.json')
+    .then((res) => res.json())
+    .then((json) => {
+      json.forEach((info) => {
+        // Vaciamos todo el html
+        DOMcarrito.textContent = '';
+        // Quitamos los duplicados
+        const carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach((item) => {
+          // Obtenemos el item que necesitamos de la variable base de datos
+          const miItem = json.filter((itemBaseDatos) => {
+            // ¿Coincide las id? Solo puede existir un caso
+            return itemBaseDatos.id === parseInt(item);
+          });
+          // Cuenta el número de veces que se repite el producto
+          const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+            return itemId === item ? (total += 1) : total;
+          }, 0);
+          // Creamos el nodo del item del carrito
+          const miNodo = document.createElement('li');
+          miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+          miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+          // Boton de borrar
+          const miBoton = document.createElement('button');
+          miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+          miBoton.textContent = 'X';
+          miBoton.style.marginLeft = '1rem';
+          miBoton.dataset.item = item;
+          miBoton.addEventListener('click', borrarItemCarrito);
+          // Mezclamos nodos
+          miNodo.appendChild(miBoton);
+          DOMcarrito.appendChild(miNodo);
+        });
+        // Renderizamos el precio total en el HTML
+        DOMtotal.textContent = calcularTotal();
+      });
     });
-    // Cuenta el número de veces que se repite el producto
-    const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-      // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
-      return itemId === item ? (total += 1) : total;
-    }, 0);
-    // Creamos el nodo del item del carrito
-    const miNodo = document.createElement('li');
-    miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-    miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
-    // Boton de borrar
-    const miBoton = document.createElement('button');
-    miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-    miBoton.textContent = 'X';
-    miBoton.style.marginLeft = '1rem';
-    miBoton.dataset.item = item;
-    miBoton.addEventListener('click', borrarItemCarrito);
-    // Mezclamos nodos
-    miNodo.appendChild(miBoton);
-    DOMcarrito.appendChild(miNodo);
-  });
-  // Renderizamos el precio total en el HTML
-  DOMtotal.textContent = calcularTotal();
 }
 
 /**
@@ -175,11 +134,14 @@ function borrarItemCarrito(evento) {
 /**
  * Calcula el precio total teniendo en cuenta los productos repetidos
  */
-function calcularTotal() {
+async function calcularTotal() {
+  let res = await fetch('data.json');
+  let json = await res.json();
+
   // Recorremos el array del carrito
   return carrito.reduce((total, item) => {
     // De cada elemento obtenemos su precio
-    const miItem = baseDeDatos.filter((itemBaseDatos) => {
+    const miItem = json.filter((itemBaseDatos) => {
       return itemBaseDatos.id === parseInt(item);
     });
     // Los sumamos al total
@@ -201,5 +163,11 @@ function vaciarCarrito() {
 DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 
 // Inicio
-renderizarProductos();
 renderizarCarrito();
+
+Swal.fire({
+  title: 'Vianda',
+  text: 'Cuando hablamos de vianda, estamos hablando de la compra de un monton de insumos para poder producirlas (Arroz, Salsa, Carne, Condimentos). Este monto puede verse afectado por el tan conocido efecto de INFLACION. El calculo de hoy es de $200 cada vianda',
+  icon: 'info',
+  confirmButtonText: 'Excelente!',
+});
--- form.js
+++ form.js
@@ -1,47 +1,21 @@
-class Comedor {
-  constructor(nombre, cantidad, ubic) {
-    this.nombre = nombre.toUpperCase();
-    this.cantidad = parseInt(cantidad);
-    this.ubic = ubic;
-  }
-}
-/* AGREGAR UN NUEVO COMEDOR  PESTAÑA COMEDORES*/
-const miFormulario = document.getElementById('formulario');
+const btn = document.getElementById('btnGuardar');
 
-const nombreComedorr = document.getElementById('nombreComedor');
-const cantidad = document.getElementById('cantidad');
-const direc = document.getElementById('direc');
-const btnGuardar = document.getElementById('btnGuardar');
+document.getElementById('formulario').addEventListener('submit', function (event) {
+  event.preventDefault();
 
-const nuevoComedor = [];
+  btn.value = 'Enviando';
 
-btnGuardar.addEventListener('click', agregarComedor);
+  const serviceID = 'default_service';
+  const templateID = 'template_sn4frj3';
 
-
-function agregarComedor(e) {
-  //Cancelamos el comportamiento del evento
-  e.preventDefault();
-
-  if (nombreComedor.value && cantidad.value && direc.value) {
-    const newHTML = document.createElement('p');
-    newHTML.innerHTML =
-      '<h2>Se registro un nuevo comedor</h2> <br>  <h2>Los Datos son:</h2> ' +
-      '<h3>Nombre Del Comedor</h3> <br>' +
-      nombreComedor.value +
-      '<h3>Cantidad de Comensales</h3> <br>' +
-      cantidad.value +
-      '<h3>Ubicacion</h3> <br>' +
-      direc.value;
-    miFormulario.append(newHTML);
-  } else {
-    alert('Completa el formulario');
-  }
-  const comedor4 = new Comedor(nombreComedor.value, cantidad.value, direc.value);
-  const enJSON = JSON.stringify(comedor4);
-  console.log(enJSON); //PONER EL FORMULARIO COMO OBJETO
-  localStorage.setItem('comedor4', enJSON);
-  /* nombreComedor.value = '';
-  cantidad.value = '';
-  direc.value = '';  */
-}
-/* AGREGAR UN NUEVO COMEDOR  PESTAÑA COMEDORES */
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = 'Enviar';
+      alert('Tu mail fue enviado');
+    },
+    (err) => {
+      btn.value = 'ERROR';
+      alert(JSON.stringify(err));
+    }
+  );
+});
